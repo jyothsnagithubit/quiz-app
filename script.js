a8a7e753339e4a0a2e59495772f8c3ed9febd395
jyothsnagithubit <140635939+jyothsnagithubit@users.noreply.github.com>
2023-10-27 09:44:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,70 @@
+const container = document.querySelector('.container');
+const questionBox = document.querySelector('.question');
+const choicesBox = document.querySelector('.choices');
+const nextBtn = document.querySelector('.nextBtn');
+const scoreCard = document.querySelector('.scoreCard');
+const alert = document.querySelector('.alert');
+const startBtn = document.querySelector('.startBtn');
+const timer = document.querySelector('.timer');
+
+
+// Make an array of objects that stores question, choices of question and answer
+const quiz = [
+    {
+        question: "Q. Which of the following is not a CSS box model property?",
+        choices: ["margin", "padding", "border-radius", "border-collapse"],
+        answer: "border-collapse"
+    },
+    {
+        question: "Q. Which of the following is not a valid way to declare a function in JavaScript?",
+        choices: ["function myFunction() {}", " let myFunction = function() {};", "myFunction: function() {}", "const myFunction = () => {};"],
+        answer: "myFunction: function() {}"
+    },
+    {
+        question: "Q. Which of the following is not a JavaScript data type?",
+        choices: ["string", "boolean", "object", "float"],
+        answer: "float"
+    },
+    {
+        question: "Q. What is the purpose of the this keyword in JavaScript?",
+        choices: ["It refers to the current function.", "It refers to the current object.", "It refers to the parent object.", " It is used for comments."],
+        answer: "It refers to the current object."
+    }
+];
+
+// Making Variables
+let currentQuestionIndex = 0;
+let score = 0;
+let quizOver = false;
+let timeLeft = 15;
+let timerID = null;
+
+// Arrow Function to Show Questions
+const showQuestions = () => {
+    const questionDetails = quiz[currentQuestionIndex];
+    questionBox.textContent = questionDetails.question;
+
+    choicesBox.textContent = "";
+    for (let i = 0; i < questionDetails.choices.length; i++) {
+        const currentChoice = questionDetails.choices[i];
+        const choiceDiv = document.createElement('div');
+        choiceDiv.textContent = currentChoice;
+        choiceDiv.classList.add('choice');
+        choicesBox.appendChild(choiceDiv);
+
+        choiceDiv.addEventListener('click', () => {
+            if (choiceDiv.classList.contains('selected')) {
+                choiceDiv.classList.remove('selected');
+            }
+            else {
+                choiceDiv.classList.add('selected');
+            }
+        });
+    }
+
+    if(currentQuestionIndex < quiz.length){
+        startTimer();
+    }
+}
+
+// Function to check answers
